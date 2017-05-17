e8ee45ddd27187cea91d3dd4bf2258cff9236bed
vedantc98 <vedantc98@gmail.com>
2017-05-17 23:44:40 +0530

    Added debugging tools

--- responses.py
+++ responses.py
@@ -9,14 +9,17 @@ def makeWebhookResponse(req):
 	try:
 		req_result=req.get("result")
 	except:
+		print "Lol3"
 		return {"lol":"lol"}
 
 	try:
 		req_result_action=req_result.get("action")
 	except:
+		print "Lol2"
 		return {"lol2":"lol2"}
 
 	if not req_result_action. strip()=="get_joke":
+		print "LOL1"
 		return {}
 	
 	#get_joke is supposed to return a tuple containing (joke, source)
