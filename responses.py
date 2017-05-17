44940916e2e41ea4c71ca998fc83fa543536ad6a
vedantc98 <vedantc98@gmail.com>
2017-05-17 23:40:30 +0530

    Further further debugging joke handler

--- responses.py
+++ responses.py
@@ -6,7 +6,17 @@ from flask import request
 import jokes
 
 def makeWebhookResponse(req):
-	if not req.get("result").get("action").strip()=="get_joke":
+	try:
+		req_result=req.get("result")
+	except:
+		return {"lol":"lol"}
+
+	try:
+		req_result_action=req_result.get("action")
+	except:
+		return {"lol2":"lol2"}
+
+	if not req_result_action. strip()=="get_joke":
 		return {}
 	
 	#get_joke is supposed to return a tuple containing (joke, source)
@@ -27,7 +37,7 @@ def makeWebhookResponse(req):
 def makeJSON(res):
 
 	res=json.dumps(res, indent=4)
-	r=make_request(res)
+	r=make_response(res)
 
 	r.headers['Content-Type']='application/json'
 
